f75545aabcaa1ea0586648faa477f060188ae6f1
GPashov314 <GPashov314@gmail.com>
2018-03-12 11:59:24 +0200

    Add Problem

---
+++ intro-and-basic-syntax/excercise/MilesToKm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometers = double.Parse(Console.ReadLine());
+            double mile = kilometers * 1.60934;
+            Console.WriteLine($"{mile:F2}");
+        }
+    }
+}
