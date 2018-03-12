b62daea52f8555cafa6c8a362a645937e868374e
tania92lp <plamen4417>
2018-03-12 12:18:35 +0200

    Added some comments

--- intro-and-basic-syntax/excercise/02.RectangleArea.cs
+++ intro-and-basic-syntax/excercise/02.RectangleArea.cs
@@ -8,8 +8,10 @@ namespace _02.RectangleArea
         {
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+            //Here we calculate area of rectangle
             double area = width * height;
 
+            //Print area
             Console.WriteLine("{0:f2}", area);
         }
     }
