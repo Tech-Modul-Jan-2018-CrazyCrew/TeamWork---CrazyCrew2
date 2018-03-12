c0adf0c968a0fb3a067febe2491955f06c84f1b7
tania92lp <plamen4417>
2018-03-12 12:44:43 +0200

    d.	Change a variable name

--- intro-and-basic-syntax/excercise/04.Beverage Labels.cs
+++ intro-and-basic-syntax/excercise/04.Beverage Labels.cs
@@ -7,13 +7,13 @@ namespace BeverageLabels
         static void Main(string[] args)
         {
             //This is  input
-            string name = Console.ReadLine();
+            string productName = Console.ReadLine();
             double volume = double.Parse(Console.ReadLine());
             double energyPer100 = double.Parse(Console.ReadLine());
             double sugarPer100 = double.Parse(Console.ReadLine());
             //Here we calculate calories
-            double energy = energyPer100 * volume / 100;
-            double sugar = sugarPer100 * volume / 100;
+            double energyContent = energyPer100 * volume / 100;
+            double sugarContent = sugarPer100 * volume / 100;
             //This is output
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{energy}kcal, {sugar}g sugars");
